a6b5b545f43c710ac913a73b187d91f5b1aade1a
Thara <chotharatape@gmail.com>
2026-04-09 14:53:43 +0700

    adjust colour

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,19 @@
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-center py-32 px-16 bg-white dark:bg-black">
+    <div className="flex flex-col flex-1 items-center justify-center font-sans">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-center py-32 px-16">
         <div className="flex flex-col items-center gap-6 text-center">
-          <h1 className="text-6xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+          <h1 className="text-7xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             Numerical Method
           </h1>
           <div className="w-full max-w-md">
-            <label className="block text-xl font-medium text-zinc-700 dark:text-zinc-300 mb-2">
+            <label className="block text-3xl font-medium text-zinc-700 dark:text-zinc-300 mb-2">
               made by Tharatape Saelee 6604062636305
             </label>
             <label className="block text-xl font-medium text-zinc-700 dark:text-zinc-300 mb-2 mt-6">
               Select Your calculation method
+            </label>
+            <label className="block text-xl font-medium text-zinc-700 dark:text-zinc-300 mb-2 mt-6">
             </label>
             <select
               className="w-full px-4 py-2 border border-zinc-300 rounded-lg text-black dark:bg-zinc-800 dark:border-zinc-600 dark:text-white focus:outline-none focus:ring-2 focus:ring-zinc-500"
@@ -24,6 +26,7 @@ export default function Home() {
               <option value="rk4">Runge-Kutta 4th Order</option>
             </select>
 
+            
             <label className="block text-xl font-medium text-zinc-700 dark:text-zinc-300 mb-2 mt-6">
             </label>
             
